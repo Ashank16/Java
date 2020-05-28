54f64ed652707d317095eaf2a7c9b38cdda914c0
Ashank <54265111+Ashank16@users.noreply.github.com>
2020-05-29 03:10:27 +0530

    Add files via upload

---
+++ Java II/1.Booleans and Conditions/1.Booleans and Comparison Operators/Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    // Print true
+    System.out.println(true);
+    
+    // Print false
+    System.out.println(false);
+    
+    // Using ==, print the result of comparing the values
+    System.out.println(12 / 4 == 3);
+    
+    // Using !=, print the result of comparing the values
+    System.out.println(12 / 4 != 3);
+    
+    // Assign the result of 3 * 9 == 27 to the bool variable
+    boolean bool = 3 * 9 == 27;
+    
+    // Print the value of the bool variable
+    System.out.println(bool);
+    
+  }
+}
---
+++ Java IV/3.Introduce Yourself with Java/11.Instance Methods(1)/Person.java
@@ -0,0 +1,26 @@
+class Person {
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
+
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+  
+  // Define the fullName method
+  public String fullName(){
+    return firstName + " " + lastName;
+  }
+  
+  // Define the bmi method
+  public double bmi(){
+    return weight / height / height;
+  }
+  
+}
